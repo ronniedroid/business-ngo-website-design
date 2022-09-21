7f0c2e0c7ab1c90e973601983b90b1881d6a58bb
ronniedroid <ronni.n.yousif@gmail.com>
2022-09-21 13:13:03 +0300

    added astro/images to optimize the images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
-
 import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), vue(), sitemap()],
+  integrations: [mdx(), vue(), sitemap(), image()],
   vite: {
     ssr: {
       noExternal: ["open-props"],
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
