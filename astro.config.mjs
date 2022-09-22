429aefac8d56028799916421622d4a86b1eb1cca
ronniedroid <ronni.n.yousif@gmail.com>
2022-09-22 08:23:29 +0300

    disabled xfbml in the js sdk

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
