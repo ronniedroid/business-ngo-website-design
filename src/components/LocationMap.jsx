974804ee98aa60378f5450a213a188da936000dd
ronniedroid <ronni.n.yousif@gmail.com>
2022-11-17 22:49:20 +0300

    made all cards match the same style

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -11,7 +11,7 @@ function LocationMap() {
       marginheight="0"
       marginwidth="0"
       src="https://www.openstreetmap.org/export/embed.html?bbox=42.94779360294343%2C36.850114363968906%2C42.95675218105316%2C36.85401198673348&layer=mapnik&marker=36.852063200192035%2C42.95227289199829"
-      style="border: 0"
+      style="border: 0; border-radius: var(--radius-3);"
       loading="lazy"
     ></iframe>
   );
