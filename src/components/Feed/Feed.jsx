6b98ab59c141274e8b79e2dd0a8107543b54b60f
Ronnie Nissan <ronnie.nissan@proton.me>
2023-05-10 09:48:37 +0300

    changed facebook page embed to show only header on mobile

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,17 +1,32 @@
-import { h } from "preact";
+import { h, Fragment } from "preact";
 import "./feed-styles.css";
 
 function Feed() {
   return (
-    <iframe
-      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHarikar2004%2F&tabs=timeline&width=400&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
-      width="400"
-      height="500"
-      style="border:none;overflow:hidden"
-      frameborder="0"
-      allowfullscreen="true"
-      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-    ></iframe>
+    <Fragment>
+      <div class="smAndUp">
+        <iframe
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHarikar2004%2F&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
+          width="500"
+          height="500"
+          style="border:none;overflow:hidden"
+          frameborder="0"
+          allowfullscreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+      <div class="smAndDown">
+        <iframe
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHarikar2004%2F&tabs=timeline&width=350&height=200&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
+          width="350"
+          height="200"
+          style="border:none;overflow:hidden"
+          frameborder="0"
+          allowfullscreen="true"
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+    </Fragment>
   );
 }
 
