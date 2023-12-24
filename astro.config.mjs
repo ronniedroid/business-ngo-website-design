a9a40dde57760e57beb644d1775fdd1faaf9d59d
ronniedroid <ronnie.nissan@proton.me>
2023-12-24 10:15:47 +0300

    upgraded to astro 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), image(), preact({ compat: true })],
+  integrations: [mdx(), preact({ compat: true })],
   vite: {
     ssr: {
       noExternal: ["open-props"],
