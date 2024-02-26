3c5a60786e3b927cf135c219dd6a314de158b507
ronniedroid <ronnie.nissan@proton.me>
2024-02-26 09:53:49 +0300

    optimizing hero image loading

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -6,6 +6,7 @@ function Feed() {
     <Fragment>
       <div class="smAndUp">
         <iframe
+          title="facebook feed"
           src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHarikar2004%2F&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
           width="500"
           height="500"
@@ -18,6 +19,7 @@ function Feed() {
       </div>
       <div class="smAndDown">
         <iframe
+          title="facebook feed"
           src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHarikar2004%2F&width=350&height=200&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
           width="350"
           height="200"
