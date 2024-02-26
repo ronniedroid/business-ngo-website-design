94f368a82939db1655cd1b57a21d8c41d709d881
ronniedroid <ronnie.nissan@proton.me>
2024-02-26 11:12:52 +0300

    changed how vacancies are listed

--- src/components/Announcements/JobsCard.jsx
+++ src/components/Announcements/JobsCard.jsx
@@ -1,19 +1,7 @@
-import { h, Fragment } from "preact";
+import { h } from "preact";
 import "./announcements-styles.css";
 
 function JobsCard(props) {
-  const setColor = (dl) => {
-    const color =
-      dl > 7
-        ? "var(--blue-7)"
-        : dl >= 3
-          ? "var(--green-7)"
-          : dl < 3 && dl > 0
-            ? "var(--red-7)"
-            : "var(--gray-7)";
-    return color;
-  };
-
   const getTime = (dl) => {
     const currentDate = new Date();
     const deadLine = new Date(dl);
@@ -28,70 +16,64 @@ function JobsCard(props) {
 
   const isDisabled = getTime(props.deadline) <= 0 ? true : false;
 
-  const backgroundColor = setColor(props.deadline);
-
   return (
     <div class="card">
-      <p class="title" style={{ "background-color": backgroundColor }}>
+      <p class="title">
         {props.title}
       </p>
       <div class="content">
-        <p>
-          <span>
-            <strong>Project name: </strong>
-          </span>
-          {props.project}
+        <p class="tpname content-title">
+          Project name:
         </p>
-        <p>
-          <span>
-            <strong>Number of vacancies: </strong>
-          </span>
+        <p class="tvac content-title">
+          Number of vacancies:
+        </p>
+        <p class="tduty content-title">
+          Duty Station:
+        </p>
+        <p class="tresidence content-title">
+          Applicant's residence:
+        </p>
+        <p class="tgender content-title">
+          Preferable gender:
+        </p>
+        <p class="tcontract content-title">
+          Type of contract:
+        </p>
+        <p class="tstaring content-title">
+          Expected starting date:
+        </p>
+        <p class="tdeadline content-title">
+          Deadline:
+        </p>
+        <p class="pname">{props.project}</p>
+        <p class="vac">
           {props.vacancies}
         </p>
-        <p>
-          <span>
-            <strong>Duty Station: </strong>
-          </span>
+        <p class="duty">
           {props.projectArea}
         </p>
-        <p>
-          <span>
-            <strong>Applicant's residence: </strong>
-          </span>
+        <p class="residence">
           {props.residence}
         </p>
-        <p>
-          <span>
-            <strong>Preferable gender: </strong>
-          </span>
+        <p class="gender">
           {props.gender}
         </p>
-        <p>
-          <span>
-            <strong>Type of contract: </strong>
-          </span>
+        <p class="contract">
           {props.typeOfContract}
         </p>
-      </div>
-      <div class="dates">
-        <p class="announcement-data">
-          <span>
-            <strong>Expected starting date: </strong>
-          </span>
+        <p class="staring">
           {props.starting}
         </p>
         <p class="deadline">
-          <span>
-            <strong>Deadline: </strong>
-          </span>
           {props.deadline}
         </p>
-      </div>
+      </div >
       <div class="buttons">
         <button class="button">Read more</button>
         <button class="button" disabled={isDisabled}>Apply</button>
       </div>
-    </div>
+    </div >
   );
 }
 
